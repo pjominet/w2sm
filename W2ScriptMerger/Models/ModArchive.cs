440f15f4f21a98c358022f6be4a0c07f211e6539
Patrick Jominet <patrick.jominet@technologypartner.io>
2026-01-10 20:46:00 +0100

    made sure changes to deployment status are upated visually

--- W2ScriptMerger/Models/ModArchive.cs
+++ W2ScriptMerger/Models/ModArchive.cs
@@ -1,42 +1,60 @@
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 namespace W2ScriptMerger.Models;
 
-public partial class ModArchive
+public partial class ModArchive : INotifyPropertyChanged
 {
     private static readonly Regex NexusIdPattern = NexusIdRegex();
-    
+
     public string SourcePath { get; init; } = string.Empty;
-    
+
     public string ModName => Path.GetFileNameWithoutExtension(SourcePath);
-    
+
     public string DisplayName => GetDisplayName();
-    
+
     public string? NexusId => GetNexusId();
-    
-    public List<ModFile> Files { get; set; } = [];
-    
+
+    public List<ModFile> Files { get; } = [];
+
     public InstallLocation ModInstallLocation { get; set; } = InstallLocation.CookedPC;
-    
+
     public string StagingPath { get; set; } = string.Empty;
-    
-    public bool IsDeployed { get; set; }
-    
+
+    public bool IsDeployed
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
     [JsonIgnore]
     public bool IsLoaded { get; set; }
-    
+
     [JsonIgnore]
     public string? Error { get; set; }
-    
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private void SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return;
+        field = value;
+        OnPropertyChanged(propertyName);
+    }
+
     private string GetDisplayName()
     {
         var name = ModName;
         var match = NexusIdPattern.Match(name);
         return match.Success ? name[..match.Index].TrimEnd('-', '_', ' ') : name;
     }
-    
+
     private string? GetNexusId()
     {
         var match = NexusIdPattern.Match(ModName);
